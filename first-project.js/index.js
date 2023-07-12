a07850ae7212920ec6ecdf9ec3e5dd1578297f78
Ragini <raginiv743@gmail.com>
2023-07-12 17:16:55 +0530

    completed palindrome checker

--- first-project.js/index.js
+++ first-project.js/index.js
@@ -1,13 +1,26 @@
-const string=prompt("enter your number or word ")
-isPolidrone(string)
-function isPolidrone(string){
-    const length=string.length
-    const pattern= /[^a-zA-Z0-9\s]/g
-    string=string.toLowerCase().replace(/ /g,'').replace(pattern,'')
-    for(let i=0; i<length ;i++){
-        if(string[i]!==string[length-1-i]){
-            return console.log("it is not")
-        }
-        return console.log("it is polidrome")
+const container = document.querySelector(".container");
+const input = document.createElement("input");
+input.placeholder = "write here";
+container.appendChild(input);
+
+function isPalindrome(string) {
+  const length = string.length;
+  const pattern = /[^a-zA-Z0-9\s]/g;
+  string = string.toLowerCase().replace(/ /g, "").replace(pattern, "");
+  for (let i = 0; i < length; i++) {
+    if (string[i] !== string[length - 1 - i]) {
+      return "it is not palindrome";
     }
+    return "it is palindrome";
+  }
 }
+
+const button = document.createElement("button");
+button.innerText = "click me";
+container.appendChild(button);
+button.addEventListener("click", () => {
+  string = input.value;
+  console.log(string);
+  const result = isPalindrome(string);
+  alert(result);
+});
